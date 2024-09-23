c4bc74897a57bbd2f53617025b658fe7f7c3327f
Rene Torres <rene_edgardo_2@hotmail.com>
2024-09-22 21:14:42 -0600

    Fix issue with response validations

--- src/ses-event/dto/response/ses-event-response.dto.ts
+++ src/ses-event/dto/response/ses-event-response.dto.ts
@@ -1,25 +1,32 @@
-import { IsArray, IsBoolean, IsString } from 'class-validator';
+import { IsArray, IsBoolean, IsNotEmpty, IsString } from 'class-validator';
 
 export class SesEventResponseDto {
   @IsBoolean()
+  @IsNotEmpty()
   spam: boolean;
 
   @IsBoolean()
+  @IsNotEmpty()
   virus: boolean;
 
   @IsBoolean()
+  @IsNotEmpty()
   dns: boolean;
 
   @IsString()
+  @IsNotEmpty()
   mes: string;
 
   @IsBoolean()
+  @IsNotEmpty()
   retrasado: boolean;
 
   @IsString()
+  @IsNotEmpty()
   emisor: string;
 
   @IsArray()
   @IsString({ each: true })
+  @IsNotEmpty({ each: true })
   receptor: string[];
 }
