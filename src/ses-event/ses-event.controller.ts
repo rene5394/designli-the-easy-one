4ad98647b4f64886ebb1830786a1e8a9f2fce491
Rene Torres <rene_edgardo_2@hotmail.com>
2024-09-22 21:22:13 -0600

    Refactor adding try catch and setting properlies status code on processSesEvent function

--- src/ses-event/ses-event.controller.ts
+++ src/ses-event/ses-event.controller.ts
@@ -1,13 +1,17 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
 import { SesEventService } from './ses-event.service';
 import { SesEventRequestDto } from './dto/request/ses-event-request.dto';
+import { SesEventResponseDto } from './dto/response/ses-event-response.dto';
 
 @Controller('ses-events')
 export class SesEventController {
   constructor(private readonly sesEventService: SesEventService) {}
 
   @Post()
-  processSesEvent(@Body() createSesEventDto: SesEventRequestDto) {
+  @HttpCode(HttpStatus.OK)
+  processSesEvent(
+    @Body() createSesEventDto: SesEventRequestDto,
+  ): SesEventResponseDto {
     return this.sesEventService.processSesEvent(createSesEventDto);
   }
 }
--- src/ses-event/ses-event.service.ts
+++ src/ses-event/ses-event.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { SesEventRequestDto } from './dto/request/ses-event-request.dto';
 import { SesEventResponseDto } from './dto/response/ses-event-response.dto';
 import { extractUsername } from 'src/utils/email.utils';
@@ -6,39 +6,46 @@ import { getMonthName } from 'src/utils/date.utils';
 
 @Injectable()
 export class SesEventService {
-  processSesEvent(event: SesEventRequestDto) {
-    const { receipt, mail } = event.Records[0].ses;
-    const {
-      spamVerdict,
-      virusVerdict,
-      spfVerdict,
-      dkimVerdict,
-      dmarcPolicy,
-      processingTimeMillis,
-    } = receipt;
+  processSesEvent(event: SesEventRequestDto): SesEventResponseDto {
+    try {
+      const { receipt, mail } = event.Records[0].ses;
+      const {
+        spamVerdict,
+        virusVerdict,
+        spfVerdict,
+        dkimVerdict,
+        dmarcPolicy,
+        processingTimeMillis,
+      } = receipt;
 
-    const { timestamp, source, destination } = mail;
-    const mailMonth = getMonthName(timestamp);
+      const { timestamp, source, destination } = mail;
+      const mailMonth = getMonthName(timestamp);
 
-    const sourceUsername = extractUsername(source);
+      const sourceUsername = extractUsername(source);
 
-    const destinationUsernames = destination.map((email) => {
-      return extractUsername(email);
-    });
+      const destinationUsernames = destination.map((email) => {
+        return extractUsername(email);
+      });
 
-    const sesEventResponse: SesEventResponseDto = {
-      spam: spamVerdict.status === 'PASS',
-      virus: virusVerdict.status === 'PASS',
-      dns:
-        spfVerdict.status === 'PASS' &&
-        dkimVerdict.status === 'PASS' &&
-        dmarcPolicy === 'PASS',
-      mes: mailMonth,
-      retrasado: processingTimeMillis > 1000,
-      emisor: sourceUsername,
-      receptor: destinationUsernames,
-    };
+      const sesEventResponse: SesEventResponseDto = {
+        spam: spamVerdict.status === 'PASS',
+        virus: virusVerdict.status === 'PASS',
+        dns:
+          spfVerdict.status === 'PASS' &&
+          dkimVerdict.status === 'PASS' &&
+          dmarcPolicy === 'PASS',
+        mes: mailMonth,
+        retrasado: processingTimeMillis > 1000,
+        emisor: sourceUsername,
+        receptor: destinationUsernames,
+      };
 
-    return sesEventResponse;
+      return sesEventResponse;
+    } catch (error) {
+      throw new HttpException(
+        'Internal Server Error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
